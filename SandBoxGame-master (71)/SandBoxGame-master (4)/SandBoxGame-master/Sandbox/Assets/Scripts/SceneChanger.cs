a58455877269e355b0849a8068b79a07d6f752fd
Douglas22 <douglasalderman23@gmail.com>
2018-03-10 18:21:07 +0000

    itams added to the right layers on the bedroom scean/level

---
+++ SandBoxGame-master (71)/SandBoxGame-master (4)/SandBoxGame-master/Sandbox/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	public void Switch(string choice) 
+	{
+		switch (choice) 
+		{
+		case "startmenu":
+			Debug.Log ("Switching to StartMenu scene");
+			SceneManager.LoadScene ("Start Menu", LoadSceneMode.Single);
+			break;
+		case "bedroom":
+			Debug.Log ("Switching to Bedroom scene");
+			SceneManager.LoadScene ("Bedroom", LoadSceneMode.Single);
+			break;
+		case "lunchhall":
+			Debug.Log ("Switching to Lunch Hall scene");
+			SceneManager.LoadScene ("Lunch Hall", LoadSceneMode.Single);
+			break;
+		default:
+			Debug.Log ("Aborting scene switch because it was an unexpected scene...");
+			break;
+		}
+	}
+}
