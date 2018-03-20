2b85ea9427a88dcf3c6b63ff5e37b1d063076298
l3uddz <l3uddz@gmail.com>
2018-03-20 21:32:53 +0000

    finished incorporating tutorial example of dialog window - need to make it display instantly on screen load instead of on button press.

---
+++ Sandbox/Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalPanel : MonoBehaviour {
+
+	public Text question;
+	public Button cancelButton;
+	public Button noButton;
+	public Button yesButton;
+	public GameObject modalPanelObject;
+
+	private static ModalPanel modalPanel;
+
+	public static ModalPanel Instance () {
+		if (!modalPanel) {
+			modalPanel = FindObjectOfType(typeof (ModalPanel)) as ModalPanel;
+			if (!modalPanel)
+				Debug.LogError ("There needs to be one active ModalPanel script on a GameObject in your scene.");
+		}
+
+		return modalPanel;
+	}
+
+	// Yes/No/Cancel: A string, a Yes event, a No event and Cancel event
+	public void Choice (string question, UnityAction yesEvent, UnityAction noEvent, UnityAction cancelEvent) {
+		modalPanelObject.SetActive (true);
+
+		yesButton.onClick.RemoveAllListeners();
+		yesButton.onClick.AddListener (yesEvent);
+		yesButton.onClick.AddListener (ClosePanel);
+
+		noButton.onClick.RemoveAllListeners();
+		noButton.onClick.AddListener (noEvent);
+		noButton.onClick.AddListener (ClosePanel);
+
+		cancelButton.onClick.RemoveAllListeners();
+		cancelButton.onClick.AddListener (cancelEvent);
+		cancelButton.onClick.AddListener (ClosePanel);
+
+		this.question.text = question;
+
+		yesButton.gameObject.SetActive (true);
+		noButton.gameObject.SetActive (true);
+		cancelButton.gameObject.SetActive (true);
+	}
+
+	void ClosePanel () {
+		modalPanelObject.SetActive (false);
+	}
+
+
+}
---
+++ Sandbox/Assets/Scripts/TestModalWindow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+//  This script will be updated in Part 2 of this 2 part series.
+public class TestModalWindow : MonoBehaviour {
+	private ModalPanel modalPanel;
+	private DisplayManager displayManager;
+
+	private UnityAction myYesAction;
+	private UnityAction myNoAction;
+	private UnityAction myCancelAction;
+
+	void Awake () {
+		modalPanel = ModalPanel.Instance ();
+		displayManager = DisplayManager.Instance ();
+
+		myYesAction = new UnityAction (TestYesFunction);
+		myNoAction = new UnityAction (TestNoFunction);
+		myCancelAction = new UnityAction (TestCancelFunction);
+	}
+
+	//  Send to the Modal Panel to set up the Buttons and Functions to call
+	public void TestYNC () {
+		modalPanel.Choice ("What do you want todo today?", TestYesFunction, TestNoFunction, TestCancelFunction);
+	}
+
+	//  These are wrapped into UnityActions
+	void TestYesFunction () {
+		displayManager.DisplayMessage ("Correct answer, show schedule!");
+	}
+
+	void TestNoFunction () {
+		displayManager.DisplayMessage ("Incorrect answer...");
+	}
+
+	void TestCancelFunction () {
+		displayManager.DisplayMessage ("Incorrect answer...");
+	}
+}
